86427d6bf27c1ceaf2e723df972cb7c2bf99e6c2
Mohammad Khasati <muhammadkasati@gmail.com>
2025-03-26 17:59:08 +0200

    Update the style of the Header page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import { Button, Navbar, TextInput, Avatar, Dropdown } from "flowbite-react";
+import { Button, Navbar, TextInput, Avatar, Dropdown, Modal } from "flowbite-react";
 import Link from "next/link";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 import { useTheme } from "@/context/ThemeContext"; 
 import { useAuth } from "@/context/AuthContext";  
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 interface IProps {
   href: string;
@@ -15,7 +16,7 @@ interface IProps {
 }
 
 const NavLink = ({ href, children }: IProps) => (
-  <Link href={href} className="text-gray-700 dark:text-white hover:text-blue-500">
+  <Link href={href} className="block w-full">
     {children}
   </Link>
 );
@@ -24,6 +25,9 @@ export default function Header() {
   const { theme, toggleTheme } = useTheme();
   const { state, dispatch } = useAuth();
   const router = useRouter();
+  const path = usePathname();
+  
+  const [isSearchOpen, setIsSearchOpen] = useState(false); 
 
   const handleSignOut = async () => {
     try {
@@ -38,52 +42,78 @@ export default function Header() {
   const isLoggedIn = state.user !== null;
 
   return (
-    <Navbar className="border-b-2 bg-white dark:bg-gray-900 shadow-md">
-      <Link href="/" className="self-center text-xl font-semibold dark:text-white">
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Training
-        </span> Management
-      </Link>
+    <>
+      <Navbar className="border-b-2">
+        <Link
+          href="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            Training
+          </span>
+          Management
+        </Link>
 
-      <form className="hidden lg:block">
-        <TextInput type="text" placeholder="Search..." rightIcon={AiOutlineSearch} />
-      </form>
+        <form className="hidden lg:inline">
+          <TextInput
+            type="text"
+            placeholder="Search..."
+            rightIcon={AiOutlineSearch}
+          />
+        </form>
 
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-        <AiOutlineSearch />
-      </Button>
-
-      <div className="flex gap-3 items-center md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={toggleTheme}>
-          {theme === "light" ? <FaMoon /> : <FaSun />}
+        <Button className="w-12 h-10 lg:hidden" color="gray" pill onClick={() => setIsSearchOpen(true)}>
+          <AiOutlineSearch />
         </Button>
 
-       { isLoggedIn ? (
-          <Dropdown arrowIcon={false} inline label={<Avatar alt="user" img={state.user?.googlePhotoUrl} rounded />}>
-            <Dropdown.Header>
-              <span className="block text-sm font-semibold">@{state.user?.username}</span>
-              <span className="block text-sm text-gray-500">{state.user?.email}</span>
-            </Dropdown.Header>
-            <Dropdown.Item as={Link} href="/dashboard?tab=profile">Profile</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item as={Link} href="/dashboard?tab=dash">Dashboard</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item onClick={handleSignOut}>Sign out</Dropdown.Item>
-          </Dropdown>
-        ) : (
-          <Link href="/user/signin">
-            <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
-          </Link>
-        )}
+        <div className="flex gap-2 md:order-2">
+          <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={toggleTheme}>
+            {theme === "light" ? <FaMoon /> : <FaSun />}
+          </Button>
 
-        <Navbar.Toggle />
-      </div>
+          {isLoggedIn ? (
+            <Dropdown arrowIcon={false} inline label={<Avatar alt="user" img={state.user?.googlePhotoUrl} rounded />}>
+              <Dropdown.Header>
+                <span className="block text-sm">@{state.user?.username}</span>
+                <span className="block text-sm font-medium truncate">{state.user?.email}</span>
+              </Dropdown.Header>
+              <Dropdown.Item as={Link} href="/dashboard?tab=profile">Profile</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item as={Link} href="/dashboard?tab=dash">Dashboard</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item onClick={handleSignOut}>Sign out</Dropdown.Item>
+            </Dropdown>
+          ) : (
+            <Link href="/user/signin">
+              <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
+            </Link>
+          )}
 
-      <Navbar.Collapse>
-        <NavLink href="/">Home</NavLink>
-        <NavLink href="/about">About</NavLink>
-        <NavLink href="/support">Support</NavLink>
-      </Navbar.Collapse>
-    </Navbar>
+          <Navbar.Toggle />
+        </div>
+
+        <Navbar.Collapse>
+          <Navbar.Link active={path === "/"} as={"div"}>
+            <NavLink href="/">Home</NavLink>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/about"} as={"div"}>
+            <NavLink href="/about">About</NavLink>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/support"} as={"div"}>
+            <NavLink href="/support">Support</NavLink>
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Modal show={isSearchOpen}  onClose={() => setIsSearchOpen(false)}>
+        <Modal.Header>Search</Modal.Header>
+        <Modal.Body>
+          <TextInput type="text" placeholder="Search..." rightIcon={AiOutlineSearch} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button color="gray" onClick={() => setIsSearchOpen(false)}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
