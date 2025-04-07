6b476354d90993153eaf08d0b5b4776c3df05cf3
hadeelwadia <hadeelwadia@gmail.com>
2025-04-07 12:43:17 +0300

    update on menu component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -27,7 +27,7 @@ const Layout =({
   <div className='flex '>
    <div className=" left-0 top-0 w-64  bg-[#f8f4f3] p-4 z-50 sidebar-menu transition-transform sticky h-screen  ">
    <a href="#" className="flex items-center pb-4 border-b border-b-gray-800">
-      <h2 className="font-bold text-2xl">
+      <h2 className="font-bold text-2xl text-black">
         {state.user?.username}{""}
         <span className="bg-[#f84525] text-white px-2 rounded-md">Sup</span>
       </h2>
